06457b59c3c2cd4162b7237a4cbf16e04137b50b
nknandakuamr <tec.thunder2022nk@gmail.com>
2025-01-03 20:59:24 +0530

    Complited the remaining code of yester day

--- Day2/script.js
+++ Day2/script.js
@@ -1,20 +1,38 @@
-let todos=[]  ;
-  let inputText = document.querySelector(".add-todo input");
-  let submitbtn = document.querySelector(".add-todo button")
-   let tasks  = document.querySelector(".todos ul");
-function addTask(){
-   let task = inputText.value ;
-   if (inputText.value  === '') {
-    alert('you must write something');
-   }else{
-let li = document.createElement("li");
-li.innerHTML = inputText.value ;
-tasks.appendChild(li);
-   }
-inputText.value = '';
+let inputBox = document.querySelector("#input-box");
+let listContainer = document.querySelector("#list-container");
 
+function addTask() {
+   if(inputBox.value === ''){
+      alert("You must write something ");
+   }else{
+      let li = document.createElement("li");
+      li.innerHTML = inputBox.value ;
+      listContainer.appendChild(li);
+      let span  = document.createElement("span");
+      span.innerHTML = "x"
+      li.appendChild(span);
+   }
+   inputBox.value = "";
+   saveData();
 }
 
-submitbtn.addEventListener("click",()=>{
-  addTask();
-});
+listContainer.addEventListener("click", function(e){
+   if(e.target.tagName === "LI"){
+       e.target.classList.toggle("checked");
+       saveData();
+   }else if(e.target.tagName === "SPAN"){
+       e.target.parentElement.remove();
+       saveData();
+   }
+},false);
+
+function saveData(){
+   localStorage.setItem("datas",listContainer.innerHTML)
+  
+}
+
+function showTask(){
+   listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
